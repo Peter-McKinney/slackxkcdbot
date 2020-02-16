fade2f74361f8fe132ec4f5caec325f4b36052a6
Peter McKinney <45462076+Peter-McKinney@users.noreply.github.com>
2020-02-15 21:00:34 -0600

    working version of slack xkcd api.

--- index.js
+++ index.js
@@ -1,93 +1,83 @@
 'use strict'
 
-var SlackBot = require('slackbots');
-var Promise = require('promise');
-var util = require('util');
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var request = require('request');
 
-var xkcdbot = new SlackBot({
-  token: process.env.TOKEN || '',
-  name: process.env.NAME || 'xkcd'
-});
+var xkcdBaseUrl = 'http://xkcd.com/';
+var infoUrl = xkcdBaseUrl + 'info.0.json';
 
-var params = {
-  icon_emoji: ':xkcd:'
-};
+function generateRandomInteger(max) {
+  var id = Math.floor((Math.random() * max) + 1);
 
-//xkcdbot - returns a random xkcd comic.
-//listen for xkcd command in any channel that the bot has been invited to.
-xkcdbot.on('message', function(message) {
-  if(message.text == 'xkcd'){
-    var url = 'http://xkcd.com/';
-    var infoUrl = url + 'info.0.json';
+  //http://xkcd.com/404 displays a 404 - Not Found error page
+  while(id == 404){
+    id = Math.floor((Math.random() * max) + 1);
+  }
 
-    require('request')(infoUrl, function(error,response,body){
-      try{
-        var xkcdInfo = JSON.parse(body);
-        var id = Math.floor((Math.random() * xkcdInfo.num) + 1);
+  return id;
+}
 
-        //http://xkcd.com/404 displays a 404 - Not Found error page
-        while(id == 404){
-          id = Math.floor((Math.random() * xkcdInfo.num) + 1);
-        }
-
-        url += id + '/info.0.json';
-        requestComic(xkcdbot,message,url);
+function getCurrentComic() {
+  return new Promise((resolve, reject) => {
+    request(infoUrl, (error, response, body) => {
+      try {
+        let currentComic = JSON.parse(body);
+        resolve(currentComic);
       }
-      catch(e){
-        console.log(e);
+      catch(error) {
+        reject(error);
       }
     });
-  }
-});
-
-//uses the request module to get the json for the comic at the url
-//and uses the xkcdbot to post a message to the channel where the
-//message was received from.
-function requestComic(xkcdbot, message, url){
-  require('request')(url,function(error,result,body){
-    try{
-      console.log(body);
-      var comic = JSON.parse(body);
-
-      xkcdbot.postMessage(message.channel,
-        comic.title + '\n' + comic.img + '\n_' + comic.alt + '_',
-        params);
-    }
-    catch(e){
-      console.log('unable to send message');
-    }
   });
 }
 
-// //uses the request module and http promise module to retrieve the
-// //xkcd comic at the url. Returns the JSON comic object.
-// function requestComic(url){
-//   var promise = new Promise(function (resolve, reject){
-//     require('request')(url, function(error, response, body){
-//       if(error) reject(error);
-//
-//       try{
-//         console.log(body);
-//         var comic = JSON.parse(body);
-//
-//         resolve(comic);
-//       }
-//       catch(e){
-//         console.log('unable to retrieve comic');
-//       }
-//     })
-//   });
-//   return promise;
-// }
+async function getRandomXkcdComic() {
+  let currentComic = await getCurrentComic();
+  let randomId = generateRandomInteger(currentComic.num);
+  let url = xkcdBaseUrl + randomId + '/info.0.json';
+  let comic = await requestComic(url);
 
+  return comic;
+}
+
+function requestComic(url) {
+  return new Promise((resolve, reject) => {
+    request(url,function(error,result,body){
+      try {
+        let comic = JSON.parse(body);
+        resolve(comic);
+      }
+      catch(e){
+        reject(e);
+      }
+    });
+  });
+}
+
+app.get('/comic/random', async (req, res) => {
+  let comic = await getRandomXkcdComic();
+
+  res.status(200)
+    .contentType('application/json')
+    .send(JSON.stringify(comic));
+});
 
 app.get('/', function(req,res){
-  res.send('Slack bot app is running...');
+  res.status(200)
+    .contentType('application/json')
+    .send('Slack bot app is running...');
+});
+
+app.post('/event', (req, res) => {
+  let challenge = req.body.challenge;
+
+  res.status(200)
+    .contentType('text/plain')
+    .send(challenge);
 });
 
 app.listen(port, function(){
-  console.log('Listing on port ' + port)
+  console.log('Listing on port ' + port);
 });
